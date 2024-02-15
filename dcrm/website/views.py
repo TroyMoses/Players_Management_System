7d4cbadf820a71b8017ff4184463f32384534bc9
TroyMoses <mugabimoses07@gmail.com>
2024-02-15 13:58:14 +0300

    login

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
